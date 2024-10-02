34b2f5992fcba55f40628b0677d78e47d9a86701
Hiroyuki Katsura <moratorium08@gmail.com>
2024-10-02 23:10:21 +0900

    Add preproc test

--- src/absadt/preproc.rs
+++ src/absadt/preproc.rs
@@ -1,3 +1,11 @@
+//! preprocess [Kostyukov+ PLDI21]
+//!
+//!  - remove all the selectors and testers
+//!    - by introducing additional temporary variables
+//!  - replace dis-equality on data types with true (temporary solution)
+//!     - better solution: introduce a new predicate that expresses the dis-equality on each data type,
+//!       which is introduced by Kostyukov et al.
+//!
 use super::chc::*;
 use crate::common::*;
 
@@ -19,11 +27,12 @@ impl Polarity {
     }
 }
 
-fn handle_term(t: &term::Term, pol: Polarity) -> Term {
+/// Remove dis-equality on data types and replace equality with AdtEql
+fn handle_equality(t: &term::Term, pol: Polarity) -> Term {
     match t.get() {
         RTerm::Var(_, _) | RTerm::Cst(_) => t.clone(),
         RTerm::CArray { typ, term, .. } => {
-            let term = handle_term(term, pol);
+            let term = handle_equality(term, pol);
             term::cst_array(typ.clone(), term)
         }
         RTerm::App { op, args, .. } => {
@@ -31,7 +40,7 @@ fn handle_term(t: &term::Term, pol: Polarity) -> Term {
                 Op::Not => pol.flip(),
                 _ => pol,
             };
-            let args: Vec<_> = args.iter().map(|t| handle_term(t, pol)).collect();
+            let args: Vec<_> = args.iter().map(|t| handle_equality(t, pol)).collect();
             assert!(args.len() > 0);
             let arg = &args[0];
             match op {
@@ -51,20 +60,172 @@ fn handle_term(t: &term::Term, pol: Polarity) -> Term {
         RTerm::DTypNew {
             typ, name, args, ..
         } => {
-            let args = args.iter().map(|t| handle_term(t, pol)).collect();
+            let args = args.iter().map(|t| handle_equality(t, pol)).collect();
             term::dtyp_new(typ.clone(), name, args)
         }
         RTerm::DTypSlc { .. } => todo!(),
         RTerm::DTypTst { .. } => todo!(),
         RTerm::Fun { name, args, .. } => {
-            let args = args.iter().map(|t| handle_term(t, pol)).collect();
+            let args = args.iter().map(|t| handle_equality(t, pol)).collect();
             term::fun(name.clone(), args)
         }
     }
 }
 
+/// Remove all the selectors and testers
+fn remove_slc_tst_inner(t: &Term, v: &mut VarInfos, additional_constrs: &mut Vec<Term>) -> Term {
+    match t.get() {
+        RTerm::Var(_, _) | RTerm::Cst(_) => t.clone(),
+        RTerm::CArray { typ, term, .. } => {
+            let term = remove_slc_tst_inner(term, v, additional_constrs);
+            term::cst_array(typ.clone(), term)
+        }
+        RTerm::App {
+            depth,
+            typ,
+            op,
+            args,
+        } => {
+            let args = args
+                .iter()
+                .map(|t| remove_slc_tst_inner(t, v, additional_constrs))
+                .collect();
+            term::app(op.clone(), args)
+        }
+        RTerm::DTypNew {
+            depth,
+            typ,
+            name,
+            args,
+        } => {
+            let args = args
+                .iter()
+                .map(|t| remove_slc_tst_inner(t, v, additional_constrs))
+                .collect();
+            term::dtyp_new(typ.clone(), name, args)
+        }
+        RTerm::DTypSlc {
+            depth,
+            typ,
+            name,
+            term,
+        } => todo!(),
+        RTerm::DTypTst {
+            typ, name, term, ..
+        } => {
+            println!("name: {}", name);
+            todo!()
+        }
+        RTerm::Fun {
+            depth,
+            typ,
+            name,
+            args,
+        } => {
+            let args = args
+                .iter()
+                .map(|t| remove_slc_tst_inner(t, v, additional_constrs))
+                .collect();
+            term::fun(name.clone(), args)
+        }
+    }
+}
+
+fn remove_slc_tst(c: &mut AbsClause) {
+    let mut constrs = Vec::new();
+    let t = remove_slc_tst_inner(&c.lhs_term, &mut c.vars, &mut constrs);
+    constrs.push(t);
+    // todo: handle arguments of predicates
+    c.lhs_term = term::and(constrs);
+}
+
 fn handle_clause(c: &mut AbsClause) {
-    c.lhs_term = handle_term(&c.lhs_term, Polarity::pos());
+    c.lhs_term = handle_equality(&c.lhs_term, Polarity::pos());
+    remove_slc_tst(c);
+}
+
+/*
+pub struct AbsClause {
+    pub vars: VarInfos,
+    pub rhs: Option<(PrdIdx, Vec<VarIdx>)>,
+    pub lhs_preds: Vec<PredApp>,
+    pub lhs_term: Term,
+}
+     */
+#[test]
+fn test_remove_slc_tst() {
+    use crate::info::VarInfo;
+    let mut preds = Preds::new();
+    let p = preds.next_index();
+    let rhs = None;
+
+    // List<T> = nil | insert (head T) (tail List<T>)
+    dtyp::create_list_dtyp();
+
+    // ilist
+    let ilist = typ::dtyp(dtyp::get("List").unwrap(), vec![typ::int()].into());
+
+    // P(tail l, head l) /\ is-cons l /\ z = head l => false
+    let mut vars = VarInfos::new();
+    let l = vars.next_index();
+    let li = VarInfo::new("l", ilist.clone(), l);
+    vars.push(li);
+    let z = vars.next_index();
+    let zi = VarInfo::new("z", typ::int(), z);
+    vars.push(zi);
+
+    let arg1 = term::dtyp_slc(ilist.clone(), "tail", term::var(l, ilist.clone()));
+    let arg2 = term::dtyp_slc(typ::int(), "head", term::var(l, ilist.clone()));
+    let args: VarMap<_> = vec![arg1, arg2].into();
+    let p = super::chc::PredApp {
+        pred: p,
+        args: args.into(),
+    };
+
+    let term1 = term::dtyp_tst("insert", term::var(l, ilist.clone()));
+    let term2 = term::eq(
+        term::var(z, typ::int()),
+        term::dtyp_slc(typ::int(), "head", term::var(l, ilist.clone())),
+    );
+    let lhs_term = term::and(vec![term1, term2]);
+    let mut c = AbsClause {
+        vars,
+        rhs,
+        lhs_preds: vec![p],
+        lhs_term,
+    };
+    remove_slc_tst(&mut c);
+
+    // check if all the selectors and testers are removed from lhs_term and lhs_preds
+    fn check_no_slc_tst(t: &Term) {
+        match t.get() {
+            RTerm::Var(_, _) | RTerm::Cst(_) => {}
+            RTerm::CArray { term, .. } => check_no_slc_tst(term),
+            RTerm::App { op, args, .. } => {
+                for arg in args.iter() {
+                    check_no_slc_tst(arg);
+                }
+            }
+            RTerm::DTypNew { args, .. } => {
+                for arg in args.iter() {
+                    check_no_slc_tst(arg);
+                }
+            }
+            RTerm::DTypSlc { .. } => panic!("DTypSlc should not appear"),
+            RTerm::DTypTst { .. } => panic!("DTypTst should not appear"),
+            RTerm::Fun { args, .. } => {
+                for arg in args.iter() {
+                    check_no_slc_tst(arg);
+                }
+            }
+        }
+    }
+    for p in c.lhs_preds.iter() {
+        for arg in p.args.iter() {
+            check_no_slc_tst(arg);
+        }
+    }
+    check_no_slc_tst(&c.lhs_term);
 }
 
 pub fn work<'a>(instance: &mut AbsInstance<'a>) {
